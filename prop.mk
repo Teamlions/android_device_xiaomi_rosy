2a0dfe434c4871eeb024494a4c3f97c9a6131b1a
Muhammed Siju <msiju@codeaurora.org>
2017-12-08 20:46:06 +0530

    msm8953-common: IMSVT: Increase cache timer for conference media events.
    Increased persist.radio.schd.cache to 3500ms for allowing events
    to reach UI correctly after merge.
    
    Change-Id: Ib646d7b77b6717e42d849ff401a8befa7b32a69a
    CRs-Fixed: 2150644

--- prop.mk
+++ prop.mk
@@ -172,6 +172,7 @@ persist.radio.multisim.config=dsds \
 persist.vendor.radio.custom_ecc=1 \
 persist.vendor.radio.rat_on=combine \
 persist.vendor.radio.sib16_support=1 \
+persist.radio.schd.cache=3500 \
 ril.subscription.types=NV,RUIM \
 rild.libargs=-d/dev/smd0 \
 rild.libpath=/vendor/lib64/libril-qc-qmi-1.so \
