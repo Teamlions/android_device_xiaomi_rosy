97e01b8a51abf1acd5be46e573af180ac2323ecb
Nikesh001 <nikkataria74@gmail.com>
2018-01-14 15:10:37 +0000

    rosy: Fix unknown baseband

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -114,3 +114,11 @@ LOCAL_SRC_FILES    := init.goodix.sh
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
+# Baseband
+include $(CLEAR_VARS)
+LOCAL_MODULE            := init.baseband.sh
+LOCAL_MODULE_TAGS       := optional eng
+LOCAL_MODULE_CLASS      := ETC
+LOCAL_SRC_FILES         := etc/init.baseband.sh
+LOCAL_MODULE_PATH       := $(TARGET_OUT_VENDOR_EXECUTABLES)
+include $(BUILD_PREBUILT)
