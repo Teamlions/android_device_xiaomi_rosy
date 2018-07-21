534b995c65d56b602729092e80cd065858e63ee9
TheScarastic <warabhishek@gmail.com>
2018-07-21 04:35:55 +0000

    rosy: symlink Goodix fingerprint firmware
    * Logcat says :-
    
    "E QSEECOMAPI: Error::Cannot open the file /system/etc/firmware/goodixfp.mdt errno = 2"
    "E QSEECOMAPI: Error::Loading image failed with ret = -1"
    
    * So Symlink Goodix to Load Goodix gf_ca properly .
    
    Signed-off-by: muralivijay <muralidharan9845@gmail.com>

--- Android.mk
+++ Android.mk
@@ -27,4 +27,18 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),rosy)
 include $(call all-makefiles-under,$(LOCAL_PATH))
+
+GOODIX_IMAGES := \
+    goodixfp.b00 goodixfp.b01 goodixfp.b02 goodixfp.b03 \
+    goodixfp.b04 goodixfp.b05 goodixfp.b06 goodixfp.mdt
+
+GOODIX_SYMLINKS := $(addprefix $(TARGET_OUT_ETC)/firmware/,$(notdir $(GOODIX_IMAGES)))
+$(GOODIX_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "Goodix firmware link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /firmware/image/$(notdir $@) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(GOODIX_SYMLINKS)
+
 endif
