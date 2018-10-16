8ba1f37345e801ee5140bf5a2c4505a958ee7f85
haran <harangovindan@gmail.com>
2018-10-16 17:38:13 +0200

    rosy: audio: Increase voice call and media volume steps
    * This fixes low volume in general for voice call and media volume

--- prop.mk
+++ prop.mk
@@ -2,6 +2,11 @@
 # system.prop for rosy
 #
 
+# Audio
+PRODUCT_PROPERTY_OVERRIDES += \
+ro.config.media_vol_steps=25 \
+ro.config.vc_call_vol_steps=7
+	
 # DT2W (rosy)
 # value = 1 means enabled
 # value = 0 means disabled
