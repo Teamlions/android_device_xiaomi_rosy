c556170989a589a26cf2de790aed41608da05a9d
muralivijay <muralidharan9845@gmail.com>
2018-12-13 14:29:20 +0000

    rosy: Drop DT2W manual Switch from build.prop
    * when bringup oreo for our rosy dt2w not working by toogle swith
    * our @MacTavishAO implement this function for fix dt2w but users unable to switch dt2w on/off from (Settings-->display).
    * now in oreo added dt2w control thought power hal
    * so disable dt2w by default (forcefully) everyrom now.
    
    Fixed :- DT2W toogle from settings
    
    Signed-off-by: muralivijay <muralidharan9845@gmail.com>

--- prop.mk
+++ prop.mk
@@ -6,9 +6,3 @@
 PRODUCT_PROPERTY_OVERRIDES += \
 ro.config.media_vol_steps=25 \
 ro.config.vc_call_vol_steps=7
-	
-# DT2W (rosy)
-# value = 1 means enabled
-# value = 0 means disabled
-PRODUCT_PROPERTY_OVERRIDES += \
-dt2w.enabled=1
