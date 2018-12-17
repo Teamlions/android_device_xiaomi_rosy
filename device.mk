44330761250efa5b024c5b595bac3c5a2202d1b1
aragon12 <sm95007@gmail.com>
2018-12-17 05:13:21 +0000

    rosy: biometrics: rename to device specific name

--- device.mk
+++ device.mk
@@ -33,7 +33,7 @@ PRODUCT_COPY_FILES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service-custom
+    android.hardware.biometrics.fingerprint@2.1-service.xiaomi_rosy
 
 # Input
 PRODUCT_COPY_FILES += \
