10f5fa68b84fe2102626eaee0d6f9847af236d63
muralivijay <muralidharan9845@gmail.com>
2019-05-25 18:54:31 +0000

    rosy: Audio: Fix voice bg  noice for voicecall
    * caller voice echo when make call
    
    Signed-off-by: muralivijay <muralidharan9845@gmail.com>

--- prop.mk
+++ prop.mk
@@ -1,8 +1,8 @@
 # Audio
 PRODUCT_PROPERTY_OVERRIDES += \
-persist.vendor.audio.fluence.speaker=false \
+persist.vendor.audio.fluence.speaker=true \
 persist.vendor.audio.fluence.voicecall=true \
-persist.vendor.audio.fluence.voicerec=false \
+persist.vendor.audio.fluence.voicerec=true \
 ro.vendor.audio.sdk.fluencetype=fluence \
 persist.dirac.acs.controller=qem \
 persist.dirac.acs.storeSettings=1
