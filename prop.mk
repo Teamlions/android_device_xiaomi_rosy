4b5ae8ad8eedda22f1a27db79cc86de9f84a5e8e
muralivijay <muralidharan9845@gmail.com>
2019-06-02 04:35:11 +0530

    rosy: update some required dirac props

--- prop.mk
+++ prop.mk
@@ -6,6 +6,7 @@ persist.vendor.audio.fluence.voicerec=true \
 ro.vendor.audio.sdk.fluencetype=fluence \
 persist.dirac.acs.controller=qem \
 persist.dirac.acs.storeSettings=1 \
+ro.audio.soundfx.dirac=true \
 vendor.audio.use.dts_eagle=true \
 use.dts_eagle=true \
 hpx_send_params=1
