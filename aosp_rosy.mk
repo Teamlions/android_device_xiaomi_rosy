1a2b1158585775f604773b60f41d6c4dd97d85f8
mithravindan <37381778+mithranmaghat@users.noreply.github.com>
2019-06-10 12:34:15 +0530

    Offical bringup

--- aosp_rosy.mk
+++ aosp_rosy.mk
@@ -24,7 +24,7 @@ $(call inherit-product, device/xiaomi/rosy/device.mk)
 # Inherit some common AOSP stuff.
 TARGET_BOOT_ANIMATION_RES := 720
 TARGET_GAPPS_ARCH := arm64
-CUSTOM_BUILD_TYPE := UNOFFICIAL
+CUSTOM_BUILD_TYPE := OFFICIAL
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions
